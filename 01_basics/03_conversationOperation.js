cf6182e8e059963847c10bc28eedef6eb16a8780
Xhansahab <saifkhanfff1212@gmail.com>
2025-05-22 21:09:17 +0000

    conversion operation

---
+++ 01_basics/03_conversationOperation.js
@@ -0,0 +1,20 @@
+// let age = "33"
+// let city = "Mumbai"
+let score = false
+// const number = 34.54
+// console.table(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggeedIn = "yxz"
+let BooleanIsLoggedIn = Boolean(isLoggeedIn)
+console.log(BooleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "xyz" => true
+
+
+
