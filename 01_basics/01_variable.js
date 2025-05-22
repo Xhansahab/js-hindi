8548fa96e3c055e17eba3f800915aa1fa7ea644e
Xhansahab <saifkhanfff1212@gmail.com>
2025-05-22 20:16:22 +0000

    push the code into main accout

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 124125
+let accountEmail = "Saifkhankhan4545@gmail.com"
+var accountPassword = "07860786"
+accountCity = "New Delhi"
+let accountState;
+
+//accountid = 2
+
+console.log(accountId);
+
+/*  prefer not to use var beacause of issue in block scope and functional scope  */
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
