774df582f7401156c31386cfcce9cd7d40378f9b
Xhansahab <saifkhanfff1212@gmail.com>
2025-05-22 20:41:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert (3 + 3) //that is not work because of i'm using NodeJS
+
+console.log(3 
+    +
+     3); // code readability should be high
+
+
+console.log("Saif khan");
+
+let name  = "Sahab"
+let age = 18
+let isLoggedIn = true
+
+
+/*----------------------primitive data types----------------*/
+
+// number => 2 to power 53
+// string => ""
+// bigint => for using large number
+// boolean => true/false
+// null => stanalone value
+// undefined => 
+// symbol => unique
