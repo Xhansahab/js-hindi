d8f9826c165c00541d27f201f1746b3f09f73374
Xhansahab <saifkhanfff1212@gmail.com>
2025-05-26 19:30:17 +0000

    maths and numbers

---
+++ 01_basics/06_maths_and_Nums.js
@@ -0,0 +1,26 @@
+// const score = 400
+// console.log(score); // Here  score only give the Striing value and it does not specify this is Number.
+
+// const balance = new Number(100)
+// console.log(balance); //  But here the balance is specified that it is Number Value o/p = [Number: 100]
+
+// console.log(balance.toFixed(4));
+// console.log(balance.toString().length);
+
+// const otherNumber = 23.8976
+// console.log(otherNumber.toPrecision(1));
+//  const hndreds = 1000000
+//  console.log(hndreds.toLocaleString('en-IN'));
+ // this take default USA language so for indian language you have to write ('en-IN') 
+ 
+// ++++++++++++++++ MATHS  ++++++++++++++++++++++++++++++++++ 
+
+console.log(Math);
+console.log(Math.abs(-4)); // for absolute value we use .abs
+console.log(Math.round(5.5)); // for next round of value use .round9()
+console.log(Math.ceil(4.2)); // if upper value if either .1 or .2 increase then it take upper value
+console.log(Math.floor(4.9));
+
+
+
+
