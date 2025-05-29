98f6723f8161e6e325bebf497ce14cd4f582435d
Saif Khan <saifkhanfff1212@gmail.com>
2025-05-29 19:06:43 +0000

    maths and nums

--- 01_basics/06_maths_and_Nums.js
+++ 01_basics/06_maths_and_Nums.js
@@ -15,12 +15,22 @@
  
 // ++++++++++++++++ MATHS  ++++++++++++++++++++++++++++++++++ 
 
-console.log(Math);
-console.log(Math.abs(-4)); // for absolute value we use .abs
-console.log(Math.round(5.5)); // for next round of value use .round9()
-console.log(Math.ceil(4.2)); // if upper value if either .1 or .2 increase then it take upper value
-console.log(Math.floor(4.9));
+// console.log(Math);
+// console.log(Math.abs(-4)); // for absolute value we use .abs
+// console.log(Math.round(5.5)); // for next round of value use .round9()
+// console.log(Math.ceil(4.2)); // if upper value if either .1 or .2 increase then it take upper value
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 5, 3, 7, 9, 10));
+// console.log(Math.max(4, 5, 3, 7, 9, 10));
 
 
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 
 
