bd74c9e28924c5fbc820c00eca6b9a25b96f1e1e
Saif Khan <saifkhanfff1212@gmail.com>
2025-05-30 20:59:40 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,41 @@
+//ARRAY
+
+const myArr = [1, 2, 3, 4, 5]
+// const myHeroes = ["Muahmmad S.A", "Ali R.A"]
+
+// const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[0]);
+
+// // Array Methods
+
+// myArr.push(6)
+// myArr.push(8)
+// myArr.pop(2) //this pop method only remove the last element of the array
+// console.log(myArr);
+
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr);
+// console.log(myArr.includes(1));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+
+// Slice/Splice most used method and important for Interview pov
+
+console.log("A", myArr);
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log(("B", myArr));
+
+const myn2 = myArr.splice(1, 3)
+console.log("C", myArr);
+console.log(myn2);
+
+
+
+
+
+
