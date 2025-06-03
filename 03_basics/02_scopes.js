fdf7eb9284ff05f646f479c07d55a0a58b89c78a
Saif Khan <saifkhanfff1212@gmail.com>
2025-06-03 21:30:19 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,15 @@
+// var c = 300
+let a = 300
+if (true){
+    let a = 20
+    const b = 40
+    // var c = 60
+    console.log("INNER: ", a);
+    
+
+}
+ console.log(a);
+//   console.log(b);
+//  console.log(c);
+
+ // lecture no 22 done Succesfully...!
