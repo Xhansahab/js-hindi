ebde4d98ced69257d88257f8d2935405da3a9b9f
Saif Khan <saifkhanfff1212@gmail.com>
2025-06-03 21:30:36 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,7 +28,34 @@ function logInUserMessage(username = "sahab"){
 
 }
 //console.log(logInUserMessage("hitesh"))
-console.log(logInUserMessage("saif"))
+//console.log(logInUserMessage("saif"))
 
-// lecture 19 done...! Succesfully
+// lecture 19 done...! Succesfully Here...!
+
+function calculateCartPrice (val1, val2, ...num1){
+    return num1
+}
+//console.log(calculateCartPrice (200, 400, 500, 3000));
+
+const user = {
+    username: "Sahab",
+    price: 299
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+//handleObject(user)
+handleObject({
+    username: "Saif",
+    price: 399
+})
+ const myNewArray = [200, 300, 500, 100]
+ function returnSecondValue(getArray){
+    return getArray[1]
+ }
+ //console.log(returnSecondValue(myNewArray));
+  console.log(returnSecondValue([200, 300, 500, 100]));
 
